40633f30dd5209a1cf7d414357ca18d7ec531716
reisub0 <reisub0@gmail.com>
2023-02-06 01:47:02 +0100

    Fixes with latest implementation of wordcount

--- getWordCount.test.ts
+++ getWordCount.test.ts
@@ -1,11 +1,16 @@
 import getWordCount from "./getWordCount";
 
 test("count English texts", () => {
-    const cnt = getWordCount("Hi, this is some English texts.");
+    const cnt = getWordCount("Hi, this is some English text.");
     expect(cnt).toBe(6);
 });
 
 test("count Japanese texts", () => {
    const cnt = getWordCount("こんにちは。これは、少しの日本語のテキストです。")
    expect(cnt).toBe(24);
+});
+
+test("count English texts with apostrophe", () => {
+    const cnt = getWordCount("Hi, this might've been some English text which should work with apostrophes.");
+    expect(cnt).toBe(12);
 });
